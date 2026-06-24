bcf44258a8d1d65b284743d16266c1cc0bf1542b
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-06-24 11:43:57 +0900

    chore: support multi channels

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Classify(torch.nn.Module):
     def __init__(self, n_mels: int, out_features: int):
         super(Classify, self).__init__()
 
-        self.softmax = torch.nn.Softmax(dim=2)
+        self.softmax = torch.nn.Softmax(dim=-1)
         self.convs = []
         while n_mels % 2 == 0:
             self.convs.append(
@@ -32,8 +32,8 @@ class Classify(torch.nn.Module):
         x_0 = x_0.transpose(-1, -2)
         x_0 = self.linear(x_0)
         x_0, _indices = x_0.max(dim=-2)
+        x_0, _indices = x_0.max(dim=-2)
         x_0 = self.softmax(x_0)
-        x_0 = x_0.squeeze()
 
         return x_0
 
